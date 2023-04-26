648aaa4a99b8e5b527816bf7fe9984bc4f77abe1
Parth <parthmandale8999@gmail.com>
2023-04-27 03:09:16 +0530

    added datatype file

---
+++ Day1/DataType.js
@@ -0,0 +1,14 @@
+String 
+var myName = "Parth Mandale";
+console.log(myName);
+console.log(typeof(myName));  //This -->> typeof() will tell the name of var 
+
+Number
+var myAge = 19;
+console.log(myAge);
+console.log(typeof(myAge));
+
+Boolean
+var imparth = true;
+console.log(imparth);
+console.log(typeof(imparth));
