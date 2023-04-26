12108f7537afce2e512fd98f27a5dd62152c2fd4
Parth <parthmandale8999@gmail.com>
2023-04-27 03:13:55 +0530

    updated file

--- Day1/DataType.js
+++ Day1/DataType.js
@@ -1,14 +1,14 @@
-String 
+// String 
 var myName = "Parth Mandale";
 console.log(myName);
 console.log(typeof(myName));  //This -->> typeof() will tell the name of var 
 
-Number
+// Number
 var myAge = 19;
 console.log(myAge);
 console.log(typeof(myAge));
 
-Boolean
+// Boolean
 var imparth = true;
 console.log(imparth);
 console.log(typeof(imparth));
