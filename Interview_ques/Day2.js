2e5afd35333892e41c842efb524e776cbf68ea28
Parth <parthmandale8999@gmail.com>
2023-04-27 20:41:29 +0530

    ques

---
+++ Interview_ques/Day2.js
@@ -0,0 +1,10 @@
+///Difference Between == and ===
+
+// == only checks for if value is same or not
+// === checks for if value is same and also the datatype is same or not
+
+var a =5;
+var b ="5";
+
+console.log(a==b);  //true
+console.log(a===b); //false
