81dd6b6a344765518ec13f8fa89f4e0dec9f4aa8
Parth <parthmandale8999@gmail.com>
2023-04-27 19:05:44 +0530

    added operators

---
+++ Day2/Expressions_and_operator.js
@@ -0,0 +1,27 @@
+//Expresions ex= x + y
+
+var x =5;
+var y =5;
+
+console.log(x==y);
+console.log("Equal or not" + x==y);  //bug
+
+// Arithmetic Operators
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y); 
+
+console.log("The remainder is = " + x%y); //modulator
+
+
+// Increment and decrement operator
+
+var a = 15;
+console.log(a); //15
+var b = a++;   //postInc
+var c = ++a;  //preInc
+console.log(a);  // 17  
+console.log(b);  // 15
+console.log(c);  //17
