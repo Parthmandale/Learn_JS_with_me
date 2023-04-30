a6214a210017aff55d5bb6da42af2e305954b841
Parth <parthmandale8999@gmail.com>
2023-04-30 23:47:07 +0530

    arrays

---
+++ Day4/arrays.js
@@ -0,0 +1,18 @@
+// var friends=['bishal', 'parth', 'shubham'];
+
+// console.log(friends[1]);   
+
+// console.log(friends.length);
+
+// console.log(friends[friends.length-1])
+
+// For in loop -> in means number of elements
+var friends=['bishal', 'parth', 'shubham'];
+for(let element in friends){ 
+    console.log(element);
+}
+
+// For of loop  -> of means names
+for(let element of friends){ 
+    console.log(element);
+}
