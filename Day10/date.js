5d5e55c8d35bccc88f91fd25074ace553ae78444
Parth <parthmandale8999@gmail.com>
2023-05-14 19:04:11 +0530

    Math  Module

---
+++ Day10/date.js
@@ -0,0 +1,6 @@
+let curDate= new Date();
+
+console.log(curDate);
+console.log(curDate.toLocaleTimeString());
+console.log(curDate.toLocaleDateString());
+console.log(curDate.toLocaleString());
---
+++ Day10/math.js
@@ -0,0 +1,52 @@
+console.log(Math.PI)
+
+
+
+let num = 9.7;
+console.log(Math.round(num));
+
+
+
+console.log(Math.pow(2,3));
+console.log(2**3);
+
+
+
+console.log(Math.sqrt(25));
+
+
+
+console.log(Math.abs(-99));
+console.log(Math.abs(5-8));
+
+
+
+// Interview que -> whats th diff and common in ceil vs floor vs random
+
+
+
+console.log(Math.ceil(99.1));   //100   // ROUND UP TO UP TO ITS NEAREST if anything after decimal then it will increment it by  1
+console.log(Math.round(99.1))   // 99
+
+
+
+console.log(Math.floor(99.9)); // ROUND UP TO DOWN TO ITS NEAREST
+
+
+
+
+console.log(Math.min(99,77,23,01,0,-2,-44));
+
+console.log(Math.max(99,77,23,01,0,-2,-44));
+
+
+
+//randome num btw 0 to 1 (in decimial)
+
+console.log(Math.random()); // in decimals
+console.log(Math.random() * 10);  
+
+
+
+console.log(Math.trunc(4.6));
+console.log(Math.trunc(-99.99));   //just exclude the decimal part
